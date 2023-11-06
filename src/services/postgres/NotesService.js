15e5444711fdf15cb402abb9ca33c39fd768273b
wildan090801 <muhammadwildan334@gmail.com>
2023-11-06 08:05:32 +0700

    fix edit note

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -50,7 +50,7 @@ class NotesService {
   async editNoteById(id, { title, body, tags }) {
     const updatedAt = new Date().toISOString();
     const query = {
-      text: 'UPDATE notes SET title = $1, body = $2, tags =$e, updated_at = $4 WHERE id = $5 RETURNING id',
+      text: 'UPDATE notes SET title = $1, body = $2, tags =$3, updated_at = $4 WHERE id = $5 RETURNING id',
       values: [title, body, tags, updatedAt, id],
     };
 
